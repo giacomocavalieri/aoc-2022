1c1f028423861c7833e78da92c04b82a66aca957
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-12-06 16:09:38 +0100

    Add Day 5 input generator

---
+++ src/advent/2022/extra/day_5.clj
@@ -0,0 +1,64 @@
+(ns advent.2022.extra.day-5
+  (:require [advent.2022.day-5 :refer [move-crates]]
+            [advent.utils :refer [transpose]]
+            [clojure.string :refer [join]]))
+
+(defn random-stack [max-height]
+  (let [uppercases    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        actual-height (rand-int max-height)]
+    (repeatedly actual-height #(rand-nth uppercases))))
+
+(defn make-starting-stacks [max-height message]
+  (let [message-size (count message)
+        stacks       (repeatedly message-size #(random-stack max-height))]
+    (vec (map conj stacks message))))
+
+(defn random-move [stacks]
+  (loop []
+    (let [n-stacks      (count stacks)
+          from-index    (rand-int n-stacks)
+          from-stack    (get stacks from-index)
+          to-index      (rand-int n-stacks)
+          n-crates-from (count from-stack)
+          n-crates      (inc (rand-int n-crates-from))
+          valid-move?   (and (not= from-index to-index) (not-empty from-stack))]
+      (if valid-move? [n-crates from-index to-index] (recur)))))
+
+(defn generate-input [n-steps max-height message rearrange]
+  (let [starting-stacks (make-starting-stacks max-height message)]
+    (loop [remaining-steps n-steps
+           stacks          starting-stacks
+           moves           ()]
+      (if (<= remaining-steps 0) [stacks moves]
+          (let [[n-crates from to] (random-move stacks)
+                new-stacks   (move-crates stacks n-crates from to rearrange)
+                inverse-move [n-crates to from]]
+            (recur (dec remaining-steps) new-stacks (conj moves inverse-move)))))))
+
+(defn unlines [lines] (join "\n" lines))
+(defn unwords [words] (join " " words))
+(defn serialize-move [[n-crates from to]] (str "move " n-crates " from " (inc from) " to " (inc to)))
+(defn pad-with-nil [desired-size coll]
+  (let [coll-size (count coll)
+        pad-size  (- desired-size coll-size)
+        padding   (repeat pad-size nil)]
+    (concat padding coll)))
+
+(defn serialize-block [block] (if (nil? block) "   " (str "[" block "]")))
+(defn serialize-line [line] (unwords (map serialize-block line)))
+(defn generate-bottom-line [n-stacks]
+  (let [numbers (map str (map inc (range n-stacks)))
+        line    (join "   " numbers)]
+    (str " " line)))
+(defn serialize-stacks [stacks]
+  (let [max-height    (apply max (map count stacks))
+        padded-stacks (map #(pad-with-nil max-height %) stacks)
+        transposed    (apply transpose padded-stacks)
+        serialized-stacks (unlines (map serialize-line transposed))
+        bottom-line   (generate-bottom-line (count stacks))]
+    (str serialized-stacks "\n" bottom-line)))
+
+(defn serialize-input [[stacks moves]]
+  (let [serialized-moves  (unlines (map serialize-move moves))
+        serialized-stacks (serialize-stacks stacks)]
+    (str serialized-stacks "\n\n" serialized-moves)))
--- test/advent/2022/extra/day_5_test.clj
+++ test/advent/2022/extra/day_5_test.clj
@@ -1,11 +1,24 @@
 (ns advent.2022.extra.day-5-test
   (:require [advent.2022.day-5 :as day-5]
-            [advent.2022.extra.day-5 :as extra.day-5]
+            [advent.2022.extra.day-5 :as extra.day-5 :refer [generate-input
+                                                             serialize-input]]
             [clojure.test :refer [deftest is testing]]))
 
-(deftest ^:day-5-year-2022 test-parsed-input-generator
-  (let [message "HELLOWORLD"
-        n-steps 1000
-        input   (extra.day-5/generate-parsed-input n-steps message reverse)]
+(def default-message "HELLOWORLD")
+(defn default-input [rearrange] (generate-input 1000 20 default-message rearrange))
+
+(deftest ^:day-5-year-2022 test-input-generation
+  (let [input-part-a (default-input reverse)
+        input-part-b (default-input identity)]
     (testing "Generated input yields expected message"
-      (is (= (day-5/part-a input) message)))))
+      (is (= (day-5/part-a input-part-a) default-message))
+      (is (= (day-5/part-b input-part-b) default-message)))))
+
+(deftest ^:day-5-year-2022 test-input-serialization
+  (let [input-part-a (default-input reverse)
+        input-part-b (default-input identity)
+        serialized-input-part-a (serialize-input input-part-a)
+        serialized-input-part-b (serialize-input input-part-b)]
+    (testing "Parsing is inverse of serializing"
+      (is (= (day-5/parse-input serialized-input-part-a) input-part-a))
+      (is (= (day-5/parse-input serialized-input-part-b) input-part-b)))))
