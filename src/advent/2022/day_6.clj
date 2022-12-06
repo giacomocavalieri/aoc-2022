83c8611f3558abaa108c731cebd2cdb69455795f
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-12-06 16:09:14 +0100

    Day 6

--- src/advent/2022/day_6.clj
+++ src/advent/2022/day_6.clj
@@ -1,5 +1,5 @@
 (ns advent.2022.day-6
-  (:require [advent.utils :refer [all-different? enumerate find-first read-file]]))
+  (:require [advent.utils :refer [all-different? indexes-of read-file]]))
 
 (defn parse-input [string] (seq string))
 (def sample-input-a (parse-input "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
@@ -9,12 +9,10 @@
 (def sample-input-e (parse-input "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 (def input (parse-input (read-file "day-6.txt")))
 
-(defn valid-marker? [[_index chars]] (all-different? chars))
 (defn find-marker-position [buffer marker-size]
-  (let [windows (partition marker-size 1 buffer)
-        markers (enumerate windows marker-size)
-        [index _marker] (find-first valid-marker? markers)]
-    index))
+  (let [markers (partition marker-size 1 buffer)
+        index   (first (indexes-of all-different? markers))]
+    (+ marker-size index)))
 
 (defn part-a [input] (find-marker-position input 4))
 (defn part-b [input] (find-marker-position input 14))
--- src/advent/utils.clj
+++ src/advent/utils.clj
@@ -22,9 +22,7 @@
 (defn transpose [& xs] (apply map list xs))
 (defn find-first [predicate coll] (first (filter predicate coll)))
 (defn partition-pad [n pad coll] (partition n n (repeat pad) coll))
-(defn enumerate
-  ([coll starting] (map-indexed (fn [index x] [(+ starting index) x]) coll))
-  ([coll] (enumerate coll 0)))
+(defn indexes-of [pred coll] (keep-indexed #(when (pred %2) %1) coll))
 (defn all-different? [xs]
   (let [n-xs        (count xs)
         unique-xs   (set xs)
