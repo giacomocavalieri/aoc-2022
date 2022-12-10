12534aaee99623e3d6bc0f247742525af5850f9f
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-12-10 12:41:11 +0100

    Use a somewhat more readable output for Day 9

--- src/advent/2022/day_10.clj
+++ src/advent/2022/day_10.clj
@@ -1,5 +1,5 @@
 (ns advent.2022.day-10
-  (:require [advent.utils :refer [lines parse-int read-file unlines words]]
+  (:require [advent.utils :refer [lines parse-int read-file words]]
             [clojure.core.match :refer [match]]))
 
 (defn parse-line [string]
@@ -19,17 +19,17 @@
 
 (defn cycles [ops]
   (let [starting-register-value 1]
-    (reduce add-op-cycles [starting-register-value] ops)))
+    (reduce add-op-cycles [0 starting-register-value] ops)))
 
 (defn part-a [ops]
   (let [cycles          (cycles ops)
-        signal-strength (fn [cycle-number] (* cycle-number (get cycles (dec cycle-number))))]
+        signal-strength (fn [cycle] (* cycle (get cycles cycle)))]
     (apply + (mapv signal-strength [20 60 100 140 180 220]))))
 
 (defn lit? [pixel sprite-center] (<= (dec sprite-center) pixel (inc sprite-center)))
-(defn draw-pixel [pixel sprite-center] (if (lit? pixel sprite-center) \# \.))
+(defn draw-pixel [pixel sprite-center] (if (lit? pixel sprite-center) \█ \░))
 (defn draw-row [row] (apply str (map-indexed draw-pixel row)))
 (defn part-b [ops]
-  (let [cycles (cycles ops)
+  (let [cycles (rest (cycles ops))
         rows   (partition 40 cycles)]
-    (unlines (map draw-row rows))))
+    (map draw-row rows)))
